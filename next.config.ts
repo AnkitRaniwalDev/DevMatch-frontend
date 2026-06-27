485e56f205403da954aa94e32ee5741bd79cdb81
AnkitRaniwalDev <ankit.alw011@gmail.com>
2026-06-27 20:31:26 +0530

    force update configurations

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig= {
   /* config options here */
-};
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+}
 
+}
 export default nextConfig;
